23ea9c387450696274caa472c5460b690ef00c87
Stanislav Goralik <stasgoralik@mail.ru>
2023-08-29 22:06:43 +0300

    Factory method update

--- javacore/src/main/java/javacore/factorymethod/Main.java
+++ javacore/src/main/java/javacore/factorymethod/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         DeliveryTypeFactory factory = new DeliveryTypeFactory();
         Delivery deliveryToRussia = factory.createType("Russia");
+        deliveryToRussia.deliver();
     }
 }
